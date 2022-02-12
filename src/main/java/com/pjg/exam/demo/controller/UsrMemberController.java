bcc386ba8bed8fcd0798d6c672102178e4131eb4
whdrnal <whdrnal34@gmail.com>
2022-02-12 20:11:49 +0900

    Ut.함수 도입으로 편리성 도입

--- src/main/java/com/pjg/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/pjg/exam/demo/controller/UsrMemberController.java
@@ -49,7 +49,11 @@ public class UsrMemberController {
 		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 
 		if (id == -1) {
-			return "해당 로그인아이디는 이미 사용중입니다.";
+			return Ut.f("해당 아아디(%s)는 이미 사용중입니다.",loginId);
+		}
+		
+		if (id == -2) {
+			return Ut.f("해당 이름(%s)과 이메일은(%s) 이미 사용중입니다.", name, email);
 		}
 
 		Member member = memberService.getMemberById(id);
--- src/main/java/com/pjg/exam/demo/util/Ut.java
+++ src/main/java/com/pjg/exam/demo/util/Ut.java
@@ -15,6 +15,10 @@ public class Ut {
 		return str.trim().length() == 0;
 	
 	}
+
+	public static String f(String format, Object... args) {
+		return String.format(format, args);
+	}
 	
 
 }
