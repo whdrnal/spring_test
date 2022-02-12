38b2eae3fa3af56157f789547000597bb9a247f4
whdrnal <whdrnal34@gmail.com>
2022-02-12 20:59:17 +0900

    ResultData 적용

--- src/main/java/com/pjg/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/pjg/exam/demo/controller/UsrArticleController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pjg.exam.demo.service.ArticleService;
+import com.pjg.exam.demo.util.Ut;
 import com.pjg.exam.demo.vo.Article;
+import com.pjg.exam.demo.vo.ResultData;
 
 @Controller
 public class UsrArticleController {
@@ -35,13 +37,13 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public Object getArticle(int id) {
+	public ResultData getArticle(int id) {
 		Article article = articleService.getArticle(id);
 		if (article == null) {
-			return id + "번 게시물은 존재하지 않습니다.";
+			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
 
-		return article;
+		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id), article);
 	}
 
 	@RequestMapping("/usr/article/doDelete")
