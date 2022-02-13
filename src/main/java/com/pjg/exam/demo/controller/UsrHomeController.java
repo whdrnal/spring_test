890b50b93d6fbe55998e442de5f99532e06482d2
whdrnal <whdrnal34@gmail.com>
2022-02-13 22:52:46 +0900

    JSP 적용

--- src/main/java/com/pjg/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/pjg/exam/demo/controller/UsrHomeController.java
@@ -1,14 +1,18 @@
 
 package com.pjg.exam.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class UsrHomeController {
 	@RequestMapping("/usr/home/main")
-	@ResponseBody
-	
-	public String getString() {
-		return "안녕하세요.";
+	public String showMain() {
+		return "usr/home/main";
+	}
+
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
 	}
 }
