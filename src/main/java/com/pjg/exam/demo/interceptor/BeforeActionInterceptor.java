57961c584e419095271e68c1a4cca583db9de6b0
whdrnal <whdrnal34@gmail.com>
2022-02-17 11:57:12 +0900

    BeforeAction 인터셉터 등록

--- src/main/java/com/pjg/exam/demo/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/pjg/exam/demo/interceptor/BeforeActionInterceptor.java
@@ -7,10 +7,11 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 public class BeforeActionInterceptor implements HandlerInterceptor {
+
+
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-		System.out.println("실행되니?");
-
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+"실행되니?");
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
 }
