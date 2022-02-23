23adaa3ba10d1ae8cceaec72c45f85d876f569cb
whdrnal <whdrnal34@gmail.com>
2022-02-23 10:30:46 +0900

    멀티게시판 개념 도입, board 테이블 생성

--- db/schema.sql
+++ db/schema.sql
@@ -1,32 +1,34 @@
+# DB 생성
 DROP DATABASE IF EXISTS spring_test
 CREATE DATABASE spring_test
 USE spring_test
 
+# 게시물 테이블 생성
 CREATE TABLE article (
-	id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	regDate DATETIME NOT NULL,
-	updateDate DATETIME NOT NULL,
-	title CHAR(100),
-	`body` TEXT NOT NULL
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
 );
+# 게시물, 테스트 데이터 생성
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목 1',
+`body` = '내용 1';
 
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
-title = '제목1',
-`body`= '내용1';
+title = '제목 2',
+`body` = '내용 2';
 
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
-title = '제목2',
-`body`= '내용2';
-
-INSERT INTO article
-SET regDate = NOW(),
-updateDate = NOW(),
-title = '제목3',
-`body`= '내용3';
+title = '제목 3',
+`body` = '내용 3';
 
 # 회원 테이블 생성
 CREATE TABLE `member` (
@@ -54,7 +56,7 @@ authLevel = 7,
 `name` = '관리자',
 nickname = '관리자',
 cellphoneNo = '01011111111',
-email = 'whdrnal@naver.com';
+email = 'jangka512@gmail.com';
 
 # 회원, 테스트 데이터 생성(일반 회원)
 INSERT INTO `member`
@@ -65,8 +67,7 @@ loginPw = 'user1',
 `name` = '사용자1',
 nickname = '사용자1',
 cellphoneNo = '01011111111',
-email = 'whdrnal@naver.com';
-
+email = 'jangka512@gmail.com';
 INSERT INTO `member`
 SET regDate = NOW(),
 updateDate = NOW(),
@@ -75,12 +76,49 @@ loginPw = 'user2',
 `name` = '사용자2',
 nickname = '사용자2',
 cellphoneNo = '01011111111',
-email = 'whdrnal@naver.com';
+email = 'jangka512@gmail.com';
 
-# 게시물 테이블에 회원정보 추가~~
+# 게시물 테이블에 회원정보 추가
 ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER `updateDate`;
 
-# 기존 게시물의 장성자를 2번으로 지정
+# 기존 게시물의 작성자를 2번호으로 지정
 UPDATE article
-SET memberId =2
+SET memberId = 2
 WHERE memberId = 0;
+
+# 게시판 테이블 생성
+CREATE TABLE board (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    `code` CHAR(50) NOT NULL UNIQUE COMMENT 'notice(공지사항),free1(자유게시판1),free2(자유게시판2),...',
+    `name` CHAR(50) NOT NULL UNIQUE COMMENT '게시판 이름',
+    delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '삭제여부(0=탈퇴전,1=탈퇴)',
+    delDate DATETIME COMMENT '삭제날짜'
+);
+
+# 기본 게시판 생성
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'notice',
+`name` = '공지사항';
+
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'free1',
+`name` = '자유게시판';
+
+# 게시판 테이블에 boardId 칼럼 추가
+ALTER TABLE article ADD COLUMN boardId INT(10) UNSIGNED NOT NULL AFTER memberId;
+
+# 1, 2번 게시물을 공지사항 게시물로 지정
+UPDATE article
+SET boardId = 1
+WHERE id IN (1, 2);
+
+# 3번 게시물을 자유게시판 게시물로 지정
+UPDATE article
+SET boardId = 2
+WHERE id IN (3);
