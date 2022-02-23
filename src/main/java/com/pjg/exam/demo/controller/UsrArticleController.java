00d2b7a59a6597b2e0eab3a1cdf91739ae7d18e7
whdrnal <whdrnal34@gmail.com>
2022-02-23 12:02:14 +0900

    게시물리스트에 선택된 게시판에 관련된 게시물만 표시

--- src/main/java/com/pjg/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/pjg/exam/demo/controller/UsrArticleController.java
@@ -29,11 +29,14 @@ public class UsrArticleController {
 	
 	@RequestMapping("/usr/article/list")
 	public String showList(HttpServletRequest req, Model model, int boardId) {
-		Board board = boardService.getBoardById(boardId);
-
 		Rq rq = (Rq) req.getAttribute("rq");
+		Board board = boardService.getBoardById(boardId);
+		
+		if ( board == null ) {
+			return rq.historyBackJsOnView(Ut.f("%d번 게시판은 존재하지 않습니다.", boardId));
+		}
 
-		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId());
+		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId(), boardId);
 
 		model.addAttribute("board", board);
 		model.addAttribute("articles", articles);
