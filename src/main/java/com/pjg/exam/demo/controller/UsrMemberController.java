3a000aacae3f89dfa350d32e1c9f861f453a2f81
whdrnal <whdrnal34@gmail.com>
2022-03-03 21:36:45 +0900

    로그인 후 원래 가려면 페이지로 이동

--- src/main/java/com/pjg/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/pjg/exam/demo/controller/UsrMemberController.java
@@ -2,7 +2,9 @@ package com.pjg.exam.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.pjg.exam.demo.service.MemberService;
 import com.pjg.exam.demo.util.Ut;
 import com.pjg.exam.demo.vo.Member;
@@ -58,7 +60,7 @@ public class UsrMemberController {
 	}
 	@RequestMapping("/usr/member/doLogin")
 	@ResponseBody
-	public String doLogin(String loginId, String loginPw) {
+	public String doLogin(String loginId, String loginPw, @RequestParam(defaultValue = "/") String afterLoginUri) {
 		if (Ut.empty(loginId)) {
 			return rq.jsHistoryBack("loginId(을)를 입력해주세요.");
 		}
@@ -73,7 +75,7 @@ public class UsrMemberController {
 			return rq.jsHistoryBack("비밀번호가 일치하지 않습니다.");
 		}
 		rq.login(member);
-		return rq.jsReplace(Ut.f("%s님 환영합니다.", member.getNickname()), "/");
+		return rq.jsReplace(Ut.f("%s님 환영합니다.", member.getNickname()), afterLoginUri);
 	}
 	@RequestMapping("/usr/member/myPage")
 	public String showMyPage() {
--- src/main/java/com/pjg/exam/demo/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/pjg/exam/demo/interceptor/NeedLoginInterceptor.java
@@ -18,10 +18,9 @@ public class NeedLoginInterceptor implements HandlerInterceptor {
 	
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-		
-		if (!rq.isLogined()) {		
-			// rq.printHistoryBackJs("로그인 후 이용해주세요.");
-			rq.printReplaceJs("로그인 후 이용해주세요.","../member/login");
+		if (!rq.isLogined()) {
+			String afterLoginUri = rq.getEncodedCurrentUri();
+			rq.printReplaceJs("로그인 후 이용해주세요.", "../member/login?afterLoginUrl=" + afterLoginUri);
 			return false;
 		}
 
